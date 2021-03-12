afb67bfcd7107459fcde06e1cb92dea8dcd7861d
Ajl-24 <ajl2400@gmail.com>
2021-03-12 18:16:53 +0000

    style: added foreign keys to tables

--- src/db.py
+++ src/db.py
@@ -9,8 +9,7 @@ def create_products_table_in_cafe_db():
             postgresql = """ CREATE TABLE IF NOT EXISTS products (
                             product_id SERIAL PRIMARY KEY NOT NULL,
                             product_name VARCHAR(100) NOT NULL,
-                            price DECIMAL(6,2) NOT NULL,
-                            total_price DECIMAL(6,2) NOT NULL
+                            price DECIMAL(6,2) NOT NULL
                             ) """
             cursor.execute(postgresql)
             connection.commit()
@@ -39,7 +38,8 @@ def create_orders_table_in_cafe_db():
             postgresql = """ CREATE TABLE IF NOT EXISTS orders (
                             order_id SERIAL PRIMARY KEY NOT NULL,
                             date DATE NOT NULL,
-                            location_id INT NOT NULL
+                            location_id INT NOT NULL REFERENCES cafe_locations,
+                            total_price DECIMAL(6,2) NOT NULL
                             ) """
             cursor.execute(postgresql)
             connection.commit()
@@ -52,8 +52,8 @@ def create_products_in_orders_table_in_cafe_db():
         connection = psycopg2.connect(database='cafe_db', user='root', password='password', host='172.21.0.3', port='5432')
         with connection.cursor() as cursor:
             postgresql = """ CREATE TABLE IF NOT EXISTS products_in_orders (
-                            order_id INT NOT NULL,
-                            product_id INT NOT NULL
+                            order_id INT NOT NULL REFERENCES orders,
+                            product_id INT NOT NULL REFERENCES products
                             ) """
             cursor.execute(postgresql)
             connection.commit()
@@ -66,5 +66,3 @@ create_products_table_in_cafe_db()
 create_cafe_locations_table_in_cafe_db()
 create_orders_table_in_cafe_db()
 create_products_in_orders_table_in_cafe_db()
-
-# FOREIGN KEYS
